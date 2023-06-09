3d20f5419c4c70f49e8dbfad438ae1bb321f16ec
OlgaTabisheva <93075832+OlgaTabisheva@users.noreply.github.com>
2023-06-09 19:19:51 +0200

    add partners property

--- src/components/PartnerHeader/PartnerHeader.jsx
+++ src/components/PartnerHeader/PartnerHeader.jsx
@@ -1,15 +1,22 @@
 import {Header} from "../Header/Header";
 import user from "../../images/ArturKorolev.svg";
 import './PartnerHeader.css'
+import {useSelector} from "react-redux";
+import {useParams} from "react-router-dom";
 
 export const PartnerHeader = () => {
+
+  const partners = useSelector(store => store.users.users);
+  const {id} = useParams();
+
+  const partner = partners.find(elem => elem.id === id);
   return (
     <section>
       <div className='partner-header'>
-        <img className='partner-header__image' src={user}/>
+        <img className='partner-header__image' src={partner.avatar}/>
         <div className='partner-header__box'>
-          <h1 className='partner-header__title'>Артур Королев</h1>
-          <p className='partner-header__text'>Партнер</p>
+          <h1 className='partner-header__title'>{partner.name}</h1>
+          <p className='partner-header__text'>{partner.role}</p>
         </div>
 
       </div>
